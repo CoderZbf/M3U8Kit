e4672851206493332ea6b4dafecd28b311b231df
朱保锋 <zhubaofeng@ichunqiu.com>
2016-11-23 17:55:39 +0800

    适配Carthage

---
+++ M3U8KitFramework/M3U8KitFramework.h
@@ -0,0 +1,19 @@
+//
+//  M3U8KitFramework.h
+//  M3U8KitFramework
+//
+//  Created by 朱保锋 on 16/11/23.
+//  Copyright © 2016年 0day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M3U8KitFramework.
+FOUNDATION_EXPORT double M3U8KitFrameworkVersionNumber;
+
+//! Project version string for M3U8KitFramework.
+FOUNDATION_EXPORT const unsigned char M3U8KitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M3U8KitFramework/PublicHeader.h>
+
+
---
+++ M3U8KitFrameworkTests/M3U8KitFrameworkTests.m
@@ -0,0 +1,39 @@
+//
+//  M3U8KitFrameworkTests.m
+//  M3U8KitFrameworkTests
+//
+//  Created by 朱保锋 on 16/11/23.
+//  Copyright © 2016年 0day. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface M3U8KitFrameworkTests : XCTestCase
+
+@end
+
+@implementation M3U8KitFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
